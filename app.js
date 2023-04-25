18c35557334fa2941afb77eaca8433cf200c0b7d
Derek <derekdanielson84@gmail.com>
2023-04-25 11:11:04 -0600

    Notes added

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // insertAdjacentElement Example
+function insertElement(){
+    const filter = document.querySelector('.filter');
 
+    const h1 = document.createElement('h1');
+    h1.textContent = 'insertAdjacentElement';
+
+    filter.insertAdjacentElement('beforebegin', h1);
+}
 
 // insertAdjacentText Example
 
@@ -9,6 +16,9 @@
 
 // insertBefore Example
 
+
+
+insertAdjacentElement();
 
 /*
 <!-- beforebegin -->
