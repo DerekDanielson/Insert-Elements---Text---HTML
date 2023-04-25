e0c1364fdd14497897404ef52cfef1bb82267d17
Derek <derekdanielson84@gmail.com>
2023-04-25 11:01:24 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// insertAdjacentElement Example
+
+
+// insertAdjacentText Example
+
+
+// insertAdjacentHTML Example
+
+
+// insertBefore Example
+
+
+/*
+<!-- beforebegin -->
+<p>
+    <!-- afterbegin -->
+    foo
+    <!-- beforeend -->
+</p>
+<!-- afterend -->
+*/
