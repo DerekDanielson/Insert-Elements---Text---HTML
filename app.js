34a470d8b9825451322bb72c581daf29542e725b
Derek <derekdanielson84@gmail.com>
2023-04-25 11:20:18 -0600

    Notes added

--- app.js
+++ app.js
@@ -23,12 +23,24 @@ function insertHTML(){
 }
 
 // insertBefore Example
+function insertBeforeItem(){
+    const ul = document.querySelector('ul');
+
+    const li = document.createElement('li');
+    li.textContent = 'insertBefore';
+
+    const thirdItem = document.querySelector('li:nth-child(3)');
+
+    ul.insertBefore(li, thirdItem);
+}
+
 
 
 
 insertAdjacentElement();
 insertText();
 insertHTML();
+insertBeforeItem();
 
 /*
 <!-- beforebegin -->
